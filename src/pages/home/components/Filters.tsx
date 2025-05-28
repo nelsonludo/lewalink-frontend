d30244ccdca3e67f45005d9a591ba071e25732e1
nelsonludo <ludonelvic@gmail.com>
2025-05-28 16:02:04 +0100

    upgrade: filter added

---
+++ src/pages/home/components/Filters.tsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+
+const Filters = () => {
+  const [sort, setSort] = useState<"ascending" | "descending">("ascending");
+  const [schoolCategory, setSchoolCategory] = useState<"public" | "private">(
+    "public"
+  );
+  const [rating, setRating] = useState<number>(5);
+  return (
+    <div className="bg-white shadow-md rounded-xl p-6">
+      <div className="flex justify-between border-b border-gray-200 pb-4 ">
+        <h2 className="text-2xl font-bold ">Filters</h2>
+        <img src="/images/filter.png" alt="" className="w-6 h-6" />
+      </div>
+      <div className="w-[80%]">
+        <div className="my-2">
+          <span className="font-semibold text-gray-700">Sort</span>
+          <button
+            type="button"
+            className={`flex gap-2 items-center mt-2 px-4 py-1 rounded-xl w-full cursor-pointer ${
+              sort === "ascending" ? "bg-[#F4EAFF]" : ""
+            }`}
+            onClick={() => setSort("ascending")}
+          >
+            <img src="/images/ArrowUp.png" alt="" />
+            <span>Ascending order</span>
+          </button>
+          <button
+            type="button"
+            className={`flex gap-2 items-center mt-2 px-4 py-1 rounded-xl w-full cursor-pointer ${
+              sort === "descending" ? "bg-[#F4EAFF]" : ""
+            }`}
+            onClick={() => setSort("descending")}
+          >
+            <img src="/images/ArrowUp.png" alt="" className="rotate-180" />
+            <span>Descending order</span>
+          </button>
+        </div>
+        <div className="my-2">
+          <span className="font-semibold text-gray-700">School Categories</span>
+          <button
+            type="button"
+            className={`flex gap-2 items-center mt-2 px-4 py-1 rounded-xl w-full cursor-pointer ${
+              schoolCategory === "public" ? "bg-[#F4EAFF]" : ""
+            }`}
+            onClick={() => setSchoolCategory("public")}
+          >
+            <span>Public universities</span>
+          </button>
+          <button
+            type="button"
+            className={`flex gap-2 items-center mt-2 px-4 py-1 rounded-xl w-full cursor-pointer ${
+              schoolCategory === "private" ? "bg-[#F4EAFF]" : ""
+            }`}
+            onClick={() => setSchoolCategory("private")}
+          >
+            <span>Private universities</span>
+          </button>
+        </div>
+        <div className="my-2">
+          <span className="font-semibold text-gray-700 mt-4">
+            School Rating
+          </span>
+          {[5, 4, 3, 2, 1].map((star) => (
+            <button
+              key={star}
+              type="button"
+              className={`flex items-center rounded-lg px-4 py-1 w-full cursor-pointer ${
+                rating === star ? "bg-[#F4EAFF]" : ""
+              }`}
+              onClick={() => setRating(star)}
+            >
+              {[...Array(5)].map((_, i) => (
+                <img
+                  key={i}
+                  src={
+                    i < star ? "/images/fullStar.png" : "/images/emptyStar.png"
+                  }
+                  alt=""
+                />
+              ))}
+            </button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Filters;
--- src/pages/home/components/Univertities.tsx
+++ src/pages/home/components/Univertities.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+import Filters from "./Filters";
 
-type Props = {};
-
-const Univertities = (props: Props) => {
-  return <div>Univertities</div>;
+const Univertities = () => {
+  return (
+    <div className="bg-[#F4EAFF]">
+      <div className="w-full h-50">
+        <img src="/images/homeBanner.png" alt="" className="w-full h-full" />
+      </div>
+      <div className="flex flex-row py-5 px-10 gap-4">
+        <div className="w-[25%]">
+          <Filters />
+        </div>
+        <div className="w-[75%]"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Univertities;
